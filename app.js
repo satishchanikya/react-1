6569271a5e4b01bff0529579ed2d54ce2ab18c53
satishchanikya <mallachanikya22@gmail.com>
2023-04-10 15:31:25 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const parent =React.createElement("div",{id:"parent"},
+    React.createElement("div",{id:"child1"},
+    [React.createElement("h1",{},"I'm h1 tag"),
+    React.createElement("h2",{},"I'm h2 tag")]
+),
+    React.createElement("div",{id:"child2"},
+     [React.createElement("h1",{},"I'm h1 tag"),
+    React.createElement("h2",{},"I'm h2 tag")])
+);
+
+/*const heading =React.createElement(
+    "h1",
+    {id : "heading",xyz : "uvw"},
+    "hello World From React!");*/
+
+console.log(parent);    
+
+    const root =ReactDOM.createRoot(document.getElementById("root"));
+    root.render(parent);
